7db07859c5e5a98974c632480ca0ddf92edd6d42
Raffaele Montella <raffaele.montella@uniparthenope.it>
2023-07-24 17:44:06 +0200

    Removed backend routes and authentication

--- app/routes.py
+++ app/routes.py
@@ -34,11 +34,6 @@ log.addHandler(h)
 # Create the api object using restx
 api = Api(current_app)
 
-add_model = api.model("add", {
-    "a": fields.Integer(0),
-    "b": fields.Integer(0)
-})
-
 # Get the HTTP Basic Authentication object
 auth = HTTPBasicAuth()
 
@@ -65,65 +60,6 @@ class PublicKey(Resource):
                 public_key_filename = ".." + os.sep + public_key_filename
 
             return send_file(public_key_filename, as_attachment=True)
-
-
-@auth.verify_password
-def authenticate(username, password):
-    if username and password:
-        if username == 'admin' and password == 'password':
-            return True
-
-        if username == 'user' and password == 'password':
-            return True
-
-    return False
-
-
-# Create an api parser for public key upload
-upload_publickey_parser = api.parser()
-
-# Add parser for file storage
-upload_publickey_parser.add_argument('file', location='files', type=FileStorage, required=True,
-                                     help='DYNAMO Public Key -public.pem file')
-
-
-@api.route('/upload/publickey/<string:self_id>')
-@api.expect(upload_publickey_parser)
-class PublicKeyUpload(Resource):
-    @auth.login_required
-    def post(self, self_id):
-
-        # This is FileStorage instance
-        uploaded_file = request.files['file']
-
-        # Push the application context
-        with current_app.app_context():
-
-            # Get the public key root
-            public_key_root = current_app.config["PUBLIC_KEY_ROOT"]
-
-            # Check if the filename is relative
-            #if not os.path.isabs(public_key_root):
-            #    # Move one directory level up
-            #    public_key_root = ".." + os.sep + public_key_root
-
-            # Check if self_id starts with "vessels."
-            if not self_id.startswith("vessels."):
-
-                # Add the vessels prefix
-                self_id = "vessels." + self_id
-
-            log.debug(os.getcwd())
-            # Compose the destination file path name
-            file_path = os.path.join(public_key_root, self_id + "-public.pem")
-
-            # Save the uploaded file as the file path
-            uploaded_file.save(file_path)
-
-            # Log the status
-            log.debug("Saved public key as: " + file_path)
-
-            return {"result": "ok", "user": auth.current_user()}, 200
 
 
 # Create an api parser
@@ -187,97 +123,3 @@ class ParcelUpload(Resource):
             return {'result': 'ok'}, 200
         else:
             return {'result': 'fail', "error": "File mimetype must be application/octet-stream"}, 422
-
-
-@api.route('/lastPosition')
-class LastPosition(Resource):
-    def get(self):
-        positions = []
-
-        connection_string = current_app.config["CONNECTION_STRING"]
-        engine = create_engine(connection_string, echo=False)
-        conn = engine.connect()
-
-        try:
-            sql_string = "SELECT ctx.context, pos.timestamp, ctx.value as info, pos.position FROM public.context ctx, "\
-                         "(SELECT context, timestamp, value as position FROM public.navigation_position np1 WHERE timestamp = "\
-                         "(SELECT MAX(np2.timestamp) FROM public.navigation_position np2 WHERE np1.context = np2.context) "\
-                         "ORDER BY context) pos WHERE ctx.context = pos.context LIMIT 1;"
-            result = conn.execute(text(sql_string))
-            for row in result:
-                positions.append({
-                    "id": row[0].split(":")[-1],
-                    "timestamp": str(row[1]),
-                    "info": row[2],
-                    "position": row[3]
-                })
-            conn.close()
-        except Exception as exception:
-            log.error("SQL: " + str(exception))
-            conn.close()
-
-        return positions
-
-
-@api.route('/gpx/<string:self_id>')
-class GPX(Resource):
-    def get(self, self_id):
-        # Check if self_id starts with "vessels."
-        if not self_id.startswith("vessels."):
-            # Add the vessels prefix
-            self_id = "vessels." + self_id
-
-        query = "SELECT * FROM public.navigation_position WHERE context='" + self_id + "' "
-
-        connection_string = current_app.config["CONNECTION_STRING"]
-        engine = create_engine(connection_string, echo=False)
-        conn = engine.connect()
-
-        gpx = ET.Element("gpx", attrib={"xmlns": "http://www.topografix.com/GPX/1/1"})
-        trk = ET.SubElement(gpx, "trk")
-        trkseg = ET.SubElement(trk, "trkseg")
-
-        start = request.args.get('start')
-        end = request.args.get('end')
-        hours = request.args.get('hours')
-        minutes = request.args.get('minutes')
-        seconds = request.args.get('seconds')
-
-        try:
-            if start is not None and end is not None:
-                startTime = datetime.strptime(start, '%Y%m%dZ%H%M%S')
-                endTime = datetime.strptime(end, '%Y%m%dZ%H%M%S')
-                query += f"AND timestamp >= '{startTime}' AND timestamp <= '{endTime}' ".format(startTime, endTime)
-
-            elif start is not None and (hours is not None or minutes is not None or seconds is not None):
-                startTime = datetime.strptime(start, '%Y%m%dZ%H%M%S')
-                endTime = startTime + timedelta(hours=int(hours or 0), minutes=int(minutes or 0),
-                                                seconds=int(seconds or 0))
-                query += f"AND timestamp >= '{startTime}' AND timestamp <= '{endTime}' ".format(startTime, endTime)
-
-            elif end is not None and (hours is not None or minutes is not None or seconds is not None):
-                endTime = datetime.strptime(end, '%Y%m%dZ%H%M%S')
-                startTime = endTime - timedelta(hours=int(hours or 0), minutes=int(minutes or 0),
-                                                seconds=int(seconds or 0))
-                query += f"AND timestamp >= '{startTime}' AND timestamp <= '{endTime}' ".format(startTime, endTime)
-
-            else:
-                query += "AND timestamp >= NOW() - INTERVAL '15 minutes' "
-
-        except Exception as e:
-            query += "AND timestamp >= NOW() - INTERVAL '15 minutes' "
-
-        query += "ORDER BY timestamp ASC;"
-
-        try:
-            result = conn.execute(text(query))
-            for row in result:
-                trkpt = ET.SubElement(trkseg, "trkpt", attrib={"lat": str(row[5]), "lon": str(row[4])})
-                ET.SubElement(trkpt, "time").text = str(row[1])
-
-            conn.close()
-        except Exception as e:
-            log.error("SQL: " + str(e))
-            conn.close()
-
-        return Response(ET.tostring(gpx), mimetype='text/xml')
